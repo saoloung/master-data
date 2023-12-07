f2a8dd190f62ec667b8a7de6393940a8216f70ba
Champ <saoloung@gmail.com>
2023-12-07 12:50:36 +0700

    add logic to get product list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,47 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useProductStore } from '../stores/product'
+
+const productStore = useProductStore()
+
+onMounted(async () => {
+  await productStore.loadProducts()
+})
 
 </script>
 
 <template>
-  Home View
+  <div>
+    <h2>Product List View</h2>
+  </div>
+  <div>
+    <button>Create Product</button>
+  </div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Product ID</th>
+          <th>Product Code</th>
+          <th>Product Name</th>
+          <th>Price</th>
+          <th>Image URL</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in productStore.list">
+      <td> {{ product.id }}</td>
+      <td> {{ product.productCode }}</td>
+      <td> {{ product.productName }}</td>
+      <td> {{ product.price }}</td>
+      <td> {{ product.imageUrl }}</td>
+      <td>
+      <button>See detail</button>
+      <button>Delete</button>
+      </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
