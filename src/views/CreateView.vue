e95ee16a9c67df259333611d851a490269583804
saoloung <saoloung@yahoo.com>
2023-12-07 23:32:44 +0700

    add field and create product function

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,7 +1,48 @@
+<script setup>
+import { reactive } from 'vue'
+import { RouterLink } from 'vue-router'
+import { useProductStore } from '../stores/product'
+
+const productStore = useProductStore()
+const productData = reactive({})
+
+const createProduct = async (productData) => {
+  await productStore.createProduct(productData)
+  productData.id = productStore.selectedProduct.id
+  alert('create product complete')
+}
+
+</script>
+
 <template>
+  <div>
+    Create Product View
+    <RouterLink :to="{ name: 'product-list' }">Back</RouterLink>
     <div>
-      Create View
+      <h2>Product Master</h2>
+      <div>
+        Product ID:
+        {{ productData.id }}
+      </div>
+      <div>
+        Product code:
+        <input type="text" v-model="productData.productCode">
+      </div>
+      <div>
+        Product name:
+        <input type="text" v-model="productData.productName">
+      </div>
+      <div>
+        Price:
+        <input type="text" v-model="productData.price">
+      </div>
+      <div>
+        Image URL:
+        <input type="text" v-model="productData.imageUrl">
+      </div>
+      <button @click="createProduct(productData)">Create Product</button>
     </div>
-  </template>
+  </div>
+</template>
   
   
